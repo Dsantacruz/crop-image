e9cdf629f3541b9e287e58e487407625d80a8384
juancarlos <juanc.jara@pucp.pe>
2015-12-26 20:15:59 -0500

    without jquery ui

--- build/bundle.js
+++ build/bundle.js
@@ -46,7 +46,67 @@
 
 	'use strict';
 
-	console.log('asdf');
+	var link = document.getElementById('download');
+	var cropBtn = document.getElementById('crop');
+	var destCanvas = document.getElementById('dest');
+	var cropper = document.getElementById('cropper');
+	var srcCanvas = document.getElementById('canvas');
+	var container = document.getElementById('container');
+
+	var context = srcCanvas.getContext('2d');
+
+	var getValueProperty = function getValueProperty(style, prop) {
+	  return parseInt(style.getPropertyValue(prop), 10);
+	};
+
+	cropper.addEventListener('dragstart', function (e) {
+	  var style = window.getComputedStyle(e.target, null);
+	  var obj = {
+	    left: getValueProperty(style, 'left') - e.clientX,
+	    top: getValueProperty(style, 'top') - e.clientY
+	  };
+	  e.dataTransfer.setData('text', JSON.stringify(obj));
+	});
+
+	container.addEventListener('dragover', function (e) {
+	  e.preventDefault();
+	});
+
+	container.addEventListener('drop', function (e) {
+	  var offset = JSON.parse(e.dataTransfer.getData('text/plain'));
+	  cropper.style.left = e.clientX + offset.left + 'px';
+	  cropper.style.top = e.clientY + offset.top + 'px';
+	  e.preventDefault();
+	  crop();
+	});
+
+	cropper.addEventListener('drag', function (e) {});
+
+	link.addEventListener('click', function () {
+	  link.href = destCanvas.toDataURL();
+	  link.download = 'crop.png';
+	});
+
+	var crop = function crop() {
+	  var style = window.getComputedStyle(cropper, null);
+	  var top = getValueProperty(style, 'top');
+	  var left = getValueProperty(style, 'left');
+	  destCtx.drawImage(img, left, top, 400, 400, 0, 0, 400, 400);
+	};
+
+	cropBtn.addEventListener('click', crop);
+
+	destCanvas.height = 400;
+	destCanvas.width = 400;
+	var destCtx = destCanvas.getContext('2d');
+
+	var img = new Image();
+	img.src = 'demo.jpg';
+	img.onload = function () {
+	  srcCanvas.width = img.naturalWidth;
+	  srcCanvas.height = img.naturalHeight;
+	  context.drawImage(img, 0, 0);
+	};
 
 /***/ }
 /******/ ]);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
-console.log('asdf');
+let link = document.getElementById('download');
+let cropBtn = document.getElementById('crop');
+let destCanvas = document.getElementById('dest');
+let cropper = document.getElementById('cropper');
+let srcCanvas = document.getElementById('canvas');
+let container = document.getElementById('container');
+
+let context = srcCanvas.getContext('2d');
+
+let getValueProperty = (style, prop) => {
+  return parseInt(style.getPropertyValue(prop), 10);
+};
+
+cropper.addEventListener('dragstart', e => {
+  let style = window.getComputedStyle(e.target, null);
+  let obj = {
+    left: getValueProperty(style, 'left') - e.clientX,
+    top: getValueProperty(style, 'top') - e.clientY
+  };
+  e.dataTransfer.setData('text', JSON.stringify(obj));
+});
+
+container.addEventListener('dragover', e => {
+  e.preventDefault();
+});
+
+container.addEventListener('drop', e => {
+  let offset = JSON.parse(e.dataTransfer.getData('text/plain'));
+  cropper.style.left = (e.clientX + offset.left) + 'px';
+  cropper.style.top = (e.clientY + offset.top) + 'px';
+  e.preventDefault();
+  crop();
+});
+
+cropper.addEventListener('drag', e => {
+});
+
+link.addEventListener('click', () => {
+  link.href = destCanvas.toDataURL();
+  link.download = 'crop.png';
+});
+
+let crop = () => {
+  let style = window.getComputedStyle(cropper, null);
+  let top = getValueProperty(style, 'top');
+  let left = getValueProperty(style, 'left');
+  destCtx.drawImage(img, left, top, 400, 400, 0, 0, 400, 400);
+};
+
+cropBtn.addEventListener('click', crop);
+
+destCanvas.height = 400;
+destCanvas.width = 400;
+let destCtx = destCanvas.getContext('2d');
+
+let img = new Image();
+img.src = 'demo.jpg';
+img.onload = () => {
+  srcCanvas.width = img.naturalWidth;
+  srcCanvas.height = img.naturalHeight;
+  context.drawImage(img, 0, 0);
+};
+
