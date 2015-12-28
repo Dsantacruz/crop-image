85a76d4bdbe342c1c09eee56d664fd17e8624222
juancjara <juanc.jara@pucp.pe>
2015-12-28 15:29:51 -0500

    deploy gh pages

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,4 @@ git config user.email "juanc.jara@pucp.pe"
 git add .
 git commit -m "deploy gh pages"
 
-git push --force --quiet "https//${GH_TOKEN}@${GH_REF}" master:gh-pages > dev/null 2>&1
+git push --force --quiet "https//${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
