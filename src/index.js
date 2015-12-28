f949c00ca1497a5b9912fa5318dc032c760b2695
juancarlos <juanc.jara@pucp.pe>
2015-12-27 23:03:22 -0500

    refactored with vuejs

--- src/index.js
+++ src/index.js
@@ -1,81 +1,84 @@
 import $ from 'jquery';
 import _ from 'jquery-ui/draggable';
+import Vue from 'vue';
 
-let link = document.getElementById('download');
-let destCanvas = document.getElementById('dest');
-let cropper = document.getElementById('cropper');
-let srcCanvas = document.getElementById('source');
+let vm = new Vue({
+  el: '#app',
+  data: {
+    config: {
+      width: 300,
+      height: 400
+    },
+    srcCanvasStyle: {
+      width: '500px',
+      height: '600px'
+    }
+  },
 
-$('#cropper')
-  .draggable({containment: '#container', drag: () => {crop(cropper, img);}});
+  ready() {
+    this.img = new Image();
+    this.link = document.getElementById('download');
+    this.srcCanvas = document.getElementById('source');
+    this.destCanvas = document.getElementById('dest');
+    this.destCtx = this.destCanvas.getContext('2d');
+    this.destCanvas.width = this.config.width;
+    this.destCanvas.height = this.config.height;
+    this.cropper = $('#cropper');
+    this.cropper
+      .draggable({containment: '#container', drag: () => {this.crop();}});
+  },
 
-let config = {
-  width: 400,
-  height: 400
-};
+  methods: {
 
-destCanvas.height = 400;
-destCanvas.width = 400;
-let destCtx = destCanvas.getContext('2d');
+    fileUpload(e) {
+      let file = e.target.files[0];
+      let reader = new FileReader();
 
-document.getElementById('cropHeight')
-  .addEventListener('change', e => {
-    cropper.style.height = destCanvas.height = config.height = Number(e.target.value);
-  });
-document.getElementById('cropWidth')
-  .addEventListener('change', e => {
-    cropper.style.width = destCanvas.width = config.width = Number(e.target.value);
-  });
+      reader.onloadend = () => {
+        this.img.src = reader.result;
 
-let getValueProperty = (style, prop) => {
-  return parseInt(style.getPropertyValue(prop), 10);
-};
+        this.img.onload = () => {
+          this.srcCanvas.width = this.srcCanvasStyle.width = this.img.naturalWidth;
+          this.srcCanvas.height = this.srcCanvasStyle.height = this.img.naturalHeight;
+          this.srcCanvas
+            .getContext('2d')
+            .drawImage(this.img, 0, 0);
+          this.crop();
+        };
+      };
 
-link.addEventListener('click', () => {
-  link.href = destCanvas.toDataURL();
-  link.download = 'crop.png';
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    },
+
+    crop() {
+      let pos = this.cropper.position();
+      this.destCtx.clearRect(0, 0, this.config.width, this.config.height);
+      this.destCtx.drawImage(this.img, pos.left, pos.top, this.config.width,
+                             this.config.height, 0, 0, this.config.width,
+                             this.config.height);
+
+    },
+
+    download() {
+      this.link.href = this.destCanvas.toDataURL();
+      this.link.download = 'crop.png';
+    }
+
+  }
 });
 
-let crop = (cropper, image) => {
-  let style = window.getComputedStyle(cropper, null);
-  let top = getValueProperty(style, 'top');
-  let left = getValueProperty(style, 'left');
-  destCtx.clearRect(0, 0, config.width, config.height);
-  destCtx.drawImage(image, left, top, config.width, config.height, 0, 0,
-                    config.width, config.height);
+let update = function(prop, newVal) {
+  this.destCanvas[prop] = newVal;
+  this.crop();
 };
 
-let img = new Image();
-img.src = 'demo.jpg';
-img.onload = () => {
-  srcCanvas.width = img.naturalWidth;
-  srcCanvas.height = img.naturalHeight;
-  srcCanvas
-    .getContext('2d')
-    .drawImage(img, 0, 0);
-  crop(cropper, img);
-};
-
-
-document.getElementById('fileUpload')
-  .addEventListener('change', (e) => {
-    let file = document.querySelector('input[type=file]').files[0];
-    let reader = new FileReader();
-    reader.onloadend = () => {
-      img.src = reader.result;
-      img.onload = () => {
-        srcCanvas.width = img.naturalWidth;
-        srcCanvas.height = img.naturalHeight;
-        srcCanvas
-          .getContext('2d')
-          .drawImage(img, 0, 0);
-        crop(cropper, img);
-      };
-    };
-
-    if (file) {
-      console.log('file fond');
-      reader.readAsDataURL(file);
-    }
+let createWatcher = (prop) => {
+  vm.$watch(`config.${prop}`, function(val) {
+    update.call(this, prop, val);
   });
+};
 
+createWatcher('width');
+createWatcher('height');
