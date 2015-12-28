8e16820d4033b34cc657f70b196e73b9d9bb18f7
juancjara <juanc.jara@pucp.pe>
2015-12-28 15:52:40 -0500

    stop watching build

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ mkdir out
 cp index.html out/
 cp -R build out
 
+
+cd out
 git init
 git config user.name "juancjara"
 git config user.email "juanc.jara@pucp.pe"
@@ -15,4 +17,4 @@ git config user.email "juanc.jara@pucp.pe"
 git add .
 git commit -m "deploy gh pages"
 
-git push --force --quiet "https//${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
